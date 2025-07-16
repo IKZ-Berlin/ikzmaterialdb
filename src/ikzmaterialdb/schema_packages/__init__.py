22bb410d930f51c35be587fa7c8aab3926cc1569
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-07-16 08:35:44 +0200

    Added entry point and formatted code

--- src/ikzmaterialdb/schema_packages/__init__.py
+++ src/ikzmaterialdb/schema_packages/__init__.py
@@ -1,17 +1,14 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
 class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
     def load(self):
-        from ikzmaterialdb.schema_packages.schema_package import m_package
+        from ikzmaterialdb.schema_packages.schema import m_package
 
         return m_package
 
 
 schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+    name='Material Database Schema Package',
+    description='IKZ material database schema package entry point configuration.',
 )
