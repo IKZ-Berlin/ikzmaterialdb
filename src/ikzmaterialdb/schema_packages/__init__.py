c4f2b8e85e9b8a4f633701738e638d7d98fcfe0c
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-07-16 08:38:54 +0200

    Rename NewSchemaPackageEntryPoint to MaterialDbSchemaPackageEntryPoint for clarity

--- src/ikzmaterialdb/schema_packages/__init__.py
+++ src/ikzmaterialdb/schema_packages/__init__.py
@@ -1,14 +1,14 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class MaterialDbSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     def load(self):
         from ikzmaterialdb.schema_packages.schema import m_package
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
+schema_package_entry_point = MaterialDbSchemaPackageEntryPoint(
     name='Material Database Schema Package',
     description='IKZ material database schema package entry point configuration.',
 )
